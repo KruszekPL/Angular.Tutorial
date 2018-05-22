f5410d1a3ac48b4694b9712cbd1fb7b76f1fd526
Marcin Kruszyński <github@kruszynscy.pl>
2018-05-22 12:44:30 +0200

    Remove dead code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,12 +11,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
@@ -27,5 +21,4 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-
 }
